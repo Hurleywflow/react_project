c3fc1cac363d6cb1a8e3d754a5fce5a67913500d
Hurley_Ng@ <77094397+Hurley-Ng@users.noreply.github.com>
2021-07-09 15:55:50 +1000

    localStorage set and get data

--- react-reading-list/src/contexts/BookContext.jsx
+++ react-reading-list/src/contexts/BookContext.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 // createContext let us create all content we needing.
 
 const BookContextProvider = (props) => {
-	const [books, dispatch] = useReducer(bookReducer, []);
+	// // before add books to localStorage
+	// const [books, dispatch] = useReducer(bookReducer, []);
+	// after add books to localStorage
+	const [books, dispatch] = useReducer(bookReducer, [], () => {
+		const localData = localStorage.getItem('myBook');
+		return localData ? JSON.parse(localData) : [];
+	});
+	useEffect(() => {
+		localStorage.setItem('myBook', JSON.stringify(books));
+	}, [books]);
 
 	return (
 		<BookContext.Provider value={{ books, dispatch }}>
