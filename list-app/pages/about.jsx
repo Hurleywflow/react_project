e751473ff62055491d4d850a8cf68265970c7831
Hurley_Ng@ <77094397+Hurley-Ng@users.noreply.github.com>
2021-07-30 15:38:59 +1000

    add meta and title for head pages

--- list-app/pages/about.jsx
+++ list-app/pages/about.jsx
@@ -1,19 +1,27 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function About() {
 	return (
-		<div>
-			<h1>About</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
-				vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
-				Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
-				vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
-				Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Hurley List | About</title>
+				<meta name="keywords" content="hurley" />
+			</Head>
+			{/* this Head for title of the every single pages */}
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
+					vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
+					Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
+					vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
+					Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
+				</p>
+			</div>
+		</>
 	);
 }
--- list-app/pages/index.js
+++ list-app/pages/index.js
@@ -1,24 +1,32 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className={styles.title}>Homepage</h1>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
-				vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
-				Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
-			</p>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
-				vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
-				Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
-			</p>
-			<Link href="/hurley">
-				<a className={styles.btn}>See Hurley listing</a>
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Hurley List | Home</title>
+				<meta name="keywords" content="hurley" />
+			</Head>
+			{/* this Head for title of the every single pages */}
+			<div>
+				<h1 className={styles.title}>Homepage</h1>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
+					vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
+					Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
+				</p>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsum
+					vero enim eaque modi consectetur aut alias perspiciatis eos eveniet.
+					Autem optio quasi quas. Pariatur ex minima repellendus quas saepe?
+				</p>
+				<Link href="/hurley">
+					<a className={styles.btn}>See Hurley listing</a>
+				</Link>
+			</div>
+		</>
 	);
 }
