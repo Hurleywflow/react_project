82e1f142e4e5f6197306772c9feb6df11bb035f6
Hurley_Ng@ <77094397+Hurley-Ng@users.noreply.github.com>
2021-08-03 12:26:25 +1000

    list done

--- list-app/pages/hurley/[id].jsx
+++ list-app/pages/hurley/[id].jsx
@@ -1,28 +1,26 @@
 export const getStaticPaths = async () => {
+	// getStaticPaths to generate the Route specific individual id
 	const res = await fetch('https://jsonplaceholder.typicode.com/users');
 	const data = await res.json();
-
 	// map data to an array of path objects with params (id)
 	const paths = data.map(user => ({
 		params: { id: user.id.toString() },
 	}));
-
 	return {
 		paths,
 		fallback: false,
 	};
 };
-
 export const getStaticProps = async context => {
+	// getStaticProps to get individual user id
 	const { id } = context.params;
 	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
 	const data = await res.json();
-
 	return {
 		props: { user: data },
 	};
 };
-
+// paste props properties into Details to render output
 const Details = ({ user }) => (
 	<div>
 		<h1>{user.name}</h1>
@@ -31,5 +29,4 @@ const Details = ({ user }) => (
 		<p>{user.address.city}</p>
 	</div>
 );
-
 export default Details;
